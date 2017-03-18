af6bbd7c3fcf5b6f0f8cd9ee8749e4d596b4a37e
gannyee <ganyiwelldone@gmail.com>
2017-03-18 18:20:57 +0800

    commit

---
+++ src/main/java/Struts2/HelloWorldAction.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright 2006 The Apache Software Foundation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package Struts2;
+
+import com.opensymphony.xwork2.ActionSupport;
+import com.opensymphony.xwork2.conversion.annotations.Conversion;
+import com.opensymphony.xwork2.conversion.annotations.TypeConversion;
+import com.opensymphony.xwork2.validator.annotations.RequiredFieldValidator;
+import com.opensymphony.xwork2.validator.annotations.RequiredStringValidator;
+import com.opensymphony.xwork2.validator.annotations.Validation;
+
+import java.util.Date;
+
+@Validation()
+@Conversion()
+public class HelloWorldAction extends ActionSupport {
+
+    private Date now;
+    private String name;
+
+    public Date getDateNow() {
+        return now;
+    }
+
+    @TypeConversion(converter = "Struts2.DateConverter")
+    @RequiredFieldValidator(message = "Please enter the date")
+    public void setDateNow(Date now) {
+        this.now = now;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    @RequiredStringValidator(message = "Please enter a name", trim = true)
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String execute() throws Exception {
+        return SUCCESS;
+    }
+}
